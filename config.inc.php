36893defb55aa49a30ce13a732ac700be5f8faf4
Marlon Castro <46634512+marlonscastro@users.noreply.github.com>
2021-02-15 08:30:47 -0300

    Update config.inc com db (serviço)

--- config.inc.php
+++ config.inc.php
@@ -28,7 +28,7 @@ $i++;
 /* Authentication type */
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
 /* Server parameters */
-$cfg['Servers'][$i]['host'] = '10.238.83.92';
+$cfg['Servers'][$i]['host'] = 'db';
 $cfg['Servers'][$i]['compress'] = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
